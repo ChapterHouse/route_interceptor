e65a53afecaee36ce2129b1dc4606a28ff7d699f
Greg Howdeshell <greg.howdeshell@gmail.com>
2022-06-21 15:51:26 -0500

    add specs for FakeRequest#dsl_path

--- spec/lib/route_interceptor/fake_request_spec.rb
+++ spec/lib/route_interceptor/fake_request_spec.rb
@@ -31,9 +31,50 @@ describe RouteInterceptor::FakeRequest do
     end
   end
 
-  # describe '#dsl_path' do
-  #
-  # end
+  describe '#dsl_path' do
+    let(:route) { double('ActionDispatch::Journey::Route', ast: '/trucks(.:format)') }
+
+    context 'when route ast not blank' do
+      it 'removes the :format' do
+        allow(subject).to receive(:route).and_return(route)
+        expect(subject.dsl_path).to eq('/trucks')
+      end
+    end
+
+    context 'when route ast blank' do
+      before do
+        allow(subject).to receive(:route).and_return(nil)
+      end
+      context 'when no # in the provided path' do
+        let(:path) { '/trucks/all' }
+
+        it 'returns path' do
+          expect(subject).to receive(:path).and_return(path)
+          expect(subject.dsl_path).to eq(path)
+        end
+      end
+
+      context 'when path contains single #' do
+        let(:path) { '/trucks/#' }
+        let(:expected_path) { '/trucks/:id' }
+
+        it 'returns path' do
+          expect(subject).to receive(:path).and_return(path)
+          expect(subject.dsl_path).to eq(expected_path)
+        end
+      end
+
+      context 'when path contains multiple #' do
+        let(:path) { '/trucks/#/engine/#' }
+        let(:expected_path) { '/trucks/:truck_id/engine/:id' }
+
+        it 'returns path' do
+          expect(subject).to receive(:path).and_return(path)
+          expect(subject.dsl_path).to eq(expected_path)
+        end
+      end
+    end
+  end
 
   it '#fake?' do
     expect(subject.fake?).to be_truthy
@@ -47,7 +88,6 @@ describe RouteInterceptor::FakeRequest do
   end
 
   describe '#route' do
-    
-  end
 
+  end
 end
