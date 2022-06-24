e1a0825337f9d86263cff6de597ba3f0903f48b0
Greg Howdeshell <greg.howdeshell@gmail.com>
2022-06-24 16:17:20 -0500

    update specs

--- lib/route_interceptor/intercept_controller.rb
+++ lib/route_interceptor/intercept_controller.rb
@@ -1,11 +1,10 @@
 require 'action_controller'
+require_relative 'route_inspector'
 
 module RouteInterceptor
-    class InterceptController < ::ActionController::Base
-
-    protect_from_forgery with: :null_session
-    
+  class InterceptController < ::ActionController::Base
     include RouteInspector
+    protect_from_forgery with: :null_session
 
     def self.update_intercepts(request)
       InterceptConfiguration.fetch unless request.try(:fake?)
@@ -14,6 +13,5 @@ module RouteInterceptor
     def reprocess
       reprocess_request(request)
     end
-  
   end
 end
