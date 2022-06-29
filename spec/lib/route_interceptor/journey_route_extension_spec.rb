6c981a0b24565df964a4b7bb00de804d254600b7
Greg Howdeshell <greg.howdeshell@gmail.com>
2022-06-29 11:16:26 -0500

    add skeleton for journey_route_extension_spec.rb

---
+++ spec/lib/route_interceptor/journey_route_extension_spec.rb
@@ -0,0 +1,29 @@
+describe ActionDispatch::Journey::Route do
+  describe '#existing_constraints' do
+
+  end
+
+  describe '#inject_before' do
+
+  end
+
+  describe '#inject_after' do
+
+  end
+
+  describe '#remove' do
+
+  end
+
+  describe '#shift_precedence' do
+
+  end
+
+  describe '#to_s' do
+
+  end
+
+  describe '#inject_routes' do
+
+  end
+end
